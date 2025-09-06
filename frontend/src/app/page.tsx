7bad111879d0073c09f6578502ee8437d911f65c
Amila Wijesinghe <145129622+amila-wije@users.noreply.github.com>
2025-09-06 15:10:07 +0530

    initial setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to Bodima.lk</h1>
+      <p className="mt-4 text-lg">Your one-stop solution for buying and selling properties in Sri Lanka.</p>
+    
+    </div>
+  );
+}
