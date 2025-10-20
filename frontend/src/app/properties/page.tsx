52e95d828e1983890742a93661c1d2c399cc02dd
Amila Wijesinghe <145129622+amila-wije@users.noreply.github.com>
2025-10-20 19:58:31 +0530

    updated property ui

--- frontend/src/app/properties/page.tsx
+++ frontend/src/app/properties/page.tsx
@@ -5,32 +5,32 @@ import Card from '../../components/Propertycard'
 import cardsData from "../../components/propertylist";
 import { WordRotate } from "@/components/ui/word-rotate"
 
-
 export default function Properties() {
     const [isVisible, setIsVisible] = useState(false);  
     useEffect(() => {
         setIsVisible(true);
     }, []);
+    
     return (
-        <div className="min-h-screen bg-white-500 text-white overflow-hidden bg-gray-200 dark:bg-gray-900 transition-colors duration-300">
+        <div className="min-h-screen bg-gray-200 text-white overflow-hidden dark:bg-gray-900 transition-colors duration-300">
             <Navbar/>
             <div className="flex justify-center mt-8">
-                        <h1 className='text-gray-800 p-4 inline-block text-xl sm:text-5xl dark:text-white bg-gray-200 w-full text-center dark:bg-gray-800'>
-                        <WordRotate words={["Explore Our Properties","Find your best Partner"]} />
-                        </h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 bg-blue-50  dark:bg-gray-900 p-8">
-                    {cardsData.map((card) => (
+                <h1 className='text-gray-800 p-4 inline-block text-xl sm:text-5xl dark:text-white bg-gray-200 w-full text-center dark:bg-gray-800 rounded-lg mx-4'>
+                    <WordRotate words={["Explore Our Properties","Find your best Partner"]} />
+                </h1>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-8 bg-blue-50 dark:bg-gray-900">
+                {cardsData.map((card) => (
                     <Card
-                    key={card.id}
-                    title={card.title}
-                    for={card.for}
-                    description={card.description}
-                    image={card.image}
-                    kitchen={card.kitchen}
+                        key={card.id}
+                        title={card.title}
+                        description={card.description}
+                        image={card.image}
+                        for={card.for}  // Now matches boolean data
+                        kitchen={card.kitchen}
                     />
                 ))}
-                        </div>
-                </div>
+            </div>
+        </div>
     );
 }
